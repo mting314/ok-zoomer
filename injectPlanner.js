4a906d6b646129aa38a3dfb144ad1ad7beceddeb
mting314 <michaelting314@gmail.com>
2020-08-11 14:15:23 -0700

    chnage from innerHTML to appendChild

--- injectPlanner.js
+++ injectPlanner.js
@@ -5,19 +5,26 @@ document.body.style.backgroundColor = "orange";
 
 
 	chrome.storage.sync.get("classes", function (result) {
-		var benis = document.getElementsByClassName('planneritembox smallitem')
+		var benis = document.getElementsByClassName('planneritembox')
 		var classNames = [];
 		for (var i = 0; i < result.classes.length; i++) {
 			console.log(result.classes[i].name);
 			classNames.push(result.classes[i].name);
 		}
+		console.log(benis);
 		for (var i = 0; i < benis.length; i++) {
+			console.log(benis[i]);
 			if (classNames.includes(benis[i].innerHTML.match(regex)[0].toUpperCase())) {
 				var matchedClass = benis[i].innerHTML.match(regex)[0].toUpperCase()
 				console.log("Found a match: " + benis[i].innerHTML.match(regex)[0]);
 				// TODO: Don't use innerHTML!
-				replacingHTML = document.createElement("a")
-				benis[i].innerHTML = benis[i].innerHTML.replace(regex, findElement(result.classes, 'name', matchedClass)['time'])
+				var link = document.createElement("a")
+				link.href = 'https://www.google.com/';
+				var newtext = document.createTextNode("click here!");
+				link.appendChild(newtext);
+				benis[i].appendChild(link); 
+				console.log(benis[i].innerHTML);
+				//benis[i].innerHTML = benis[i].innerHTML.replace(regex, findElement(result.classes, 'name', matchedClass)['time'])
 			}
 		}
 
