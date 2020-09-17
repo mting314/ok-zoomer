95e64f8f712d30ddccc35990daf3c1c3c27dc73f
mting314 <michaelting314@gmail.com>
2020-09-16 22:35:36 -0700

    time zones

--- background/manageAlarms.js
+++ background/manageAlarms.js
@@ -1,6 +1,19 @@
+dateWithTimeZone = (timeZone, year, month, day, hour, minute, second) => {
+  let date = new Date(Date.UTC(year, month, day, hour, minute, second));
+
+  let utcDate = new Date(date.toLocaleString('en-US', { timeZone: "UTC" }));
+  let tzDate = new Date(date.toLocaleString('en-US', { timeZone: timeZone }));
+  let offset = utcDate.getTime() - tzDate.getTime();
+
+  date.setTime( date.getTime() + offset );
+
+  return date;
+};
+
 function createSingleClassAlarm(classObject, classDayChar) {
   chrome.storage.sync.get('leeway', function (result) {
     // read in start time and end time
+    var therenow = new Date();
     var start_time_matches = classObject.classInfo.class_strt_dt.match(/\((.*?)\)/);
     var end_time_matches = classObject.classInfo.class_last_dt.match(/\((.*?)\)/);
 
@@ -9,8 +22,12 @@ function createSingleClassAlarm(classObject, classDayChar) {
       start_time = new Date(parseInt(start_time_matches[1]));
       end_time = new Date(parseInt(end_time_matches[1]));
     } else {
-      start_time = new Date();
-      end_time = new Date();
+      start_time = new Date(therenow.toLocaleString('en-US', {
+        timeZone: "America/Los_Angeles"
+      }));
+      end_time = new Date(therenow.toLocaleString('en-US', {
+        timeZone: "America/Los_Angeles"
+      }));
       end_time.setDate(end_time.getDate() + 7 * 12); // make class artificially end in 12 weeks
     }
 
@@ -19,8 +36,10 @@ function createSingleClassAlarm(classObject, classDayChar) {
     // if we start just right now, and its before the quarter starts, start_time < target is not satisfied
     // if we start then, but we're in Week 5, we'll get spammed 15 alarms from alarms being created for
     // week 1,2,3 etc.
-    var dates =  [new Date(), start_time]
-    var now = new Date(Math.max.apply(null,dates));
+    var dates = [new Date(therenow.toLocaleString('en-US', {
+      timeZone: "America/Los_Angeles"
+    })), start_time]
+    var now = new Date(Math.max.apply(null, dates));
     var classTime = classObject.classInfo.meet_items[0].meet_strt_tm.split(':')
     var classHour = parseInt(classTime[0])
     var classMinute = parseInt(classTime[1])
@@ -29,22 +48,24 @@ function createSingleClassAlarm(classObject, classDayChar) {
     var classDay = parseDayOfWeek(classDayChar);
     var dayDifference = (((classDay - now.getDay()) % 7) + 7) % 7
 
-    var timeToRing = now.getTime() + dayDifference * 24 * 60 * 60000
-    var target = new Date(timeToRing)
+    var timeToRing = new Date(now.getTime() + dayDifference * 24 * 60 * 60000)
+    // var target = new Date(timeToRing)
+    var target = dateWithTimeZone("America/Los_Angeles", timeToRing.getFullYear(), timeToRing.getMonth(), timeToRing.getDate(), classHour, classMinute, 0)
 
-    target.setHours(classHour, classMinute, 0, 0)
-    // if (dayDifference == 0) {
-    //   if (now.getTime() - target.getTime() > result.leeway * 60 * 1000) {
-    //     target.setDate(target.getDate() + 7)
-    //   }
-    // }
+
+    // target.setHours(classHour, classMinute, 0, 0)
+    if (dayDifference == 0) {
+      if (therenow.getTime() - target.getTime() > 0) {
+        target.setDate(target.getDate() + 7)
+      }
+    }
 
 
 
     // fudge data to test alarms
     var timeObject = new Date;
-    target = new Date(timeObject.getTime() + 10000);
-    end_time.setTime(target.getTime() + 30 * 24 * 60 * 60 * 1000);
+    // target = new Date(timeObject.getTime() + 10000);
+    // end_time.setTime(target.getTime() + 30 * 24 * 60 * 60 * 1000);
     console.log([start_time, target, end_time].join(" | "));
     while (start_time < target && target < end_time) {
       console.log(classObject.zoomerID + " " + target);
@@ -65,7 +86,10 @@ async function createClassAlarm(classObject) {
 function createSinglePersonalAlarm(personalEntry, personalDayChar) {
   console.log("creating alarms for", personalEntry.entryInfo.name, personalDayChar)
   chrome.storage.sync.get('leeway', function (result) {
-    var now = new Date()
+    var thereNow = new Date();
+    var now = new Date(thereNow.toLocaleString('en-US', {
+      timeZone: "America/Los_Angeles"
+    }));
     var personalStartTime = personalEntry.entryInfo.time.split('-')[0];
     var personalHour, personalMinute;
     personalHour = parseInt(personalStartTime.split(':')[0]) + 12 * (personalStartTime.slice(-2) == "pm")
@@ -79,18 +103,19 @@ function createSinglePersonalAlarm(personalEntry, personalDayChar) {
     var personalDay = parseDayOfWeek(personalDayChar);
     var dayDifference = (((personalDay - now.getDay()) % 7) + 7) % 7
 
-    var timeToRing = now.getTime() + dayDifference * 24 * 60 * 60000
-    var target = new Date(timeToRing)
+    var timeToRing = new Date(now.getTime() + dayDifference * 24 * 60 * 60000)
+    var target = dateWithTimeZone("America/Los_Angeles", timeToRing.getFullYear(), timeToRing.getMonth(), timeToRing.getDate(), personalHour, personalMinute, 0)
+    console.log(target);
 
-    target.setHours(personalHour, personalMinute, 0, 0)
+    // target.setHours(personalHour, personalMinute, 0, 0)
     if (dayDifference == 0) {
-      if (now.getTime() - target.getTime() > 0) {
+      if (thereNow.getTime() - target.getTime() > 0) {
         target.setDate(target.getDate() + 7)
       }
     }
 
     // TODO: find way to get when term ends, and end personal entries then
-    end_time = new Date();
+    end_time = new Date(now.getTime());
     end_time.setDate(end_time.getDate() + 7 * 12); // make class artificially end in 12 weeks
 
     // // fudge data to test alarms
