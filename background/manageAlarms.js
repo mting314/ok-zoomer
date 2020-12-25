a33e7b0f02f9e0c63ea4356daffde5003a9e222c
mting314 <michaelting314@gmail.com>
2020-12-25 01:46:18 -0800

    var to let in manageAlarms

--- background/manageAlarms.js
+++ background/manageAlarms.js
@@ -1,27 +1,25 @@
 // moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0');
 
 function createAlarms(entry) {
-  if (entry.classTimes.length == 0) {
+  if (entry.classTimes.length === 0) {
     return;
   }
   moment.tz.setDefault("America/Los_Angeles");
+
   // since classes have a defined start time, and we could be adding a class after the quarter starts,
   // we have to start creating alarms from the LATER of right now, and the day classes start
   // if we start just right now, and its before the quarter starts, start_time < target is not satisfied
   // if we start then, but we're in Week 5, we'll get spammed 15 alarms from alarms being created for
   // week 1,2,3 etc.
-  // var dates = [now, new Date(entry.timeBoundaries[0])]
-  // var now = new Date(Math.max.apply(null, dates));
+  for (let i = 0; i < entry.classTimes.length; i++) {
+    let now = moment.max(moment(), moment.unix(entry.timeBoundaries[0]/1000))
+    let classTime = entry.classTimes[i].split(":");
+    let classDay = parseInt(classTime[0]);
+    let dayDifference = (((classDay - now.day()) % 7) + 7) % 7
 
-  for (var i = 0; i < entry.classTimes.length; i++) {
-    var now = moment.max(moment(), moment.unix(entry.timeBoundaries[0]/1000))
-    var classTime = entry.classTimes[i].split(":");
-    var classDay = parseInt(classTime[0]);
-    var dayDifference = (((classDay - now.day()) % 7) + 7) % 7
+    let timeToRing = now.add(dayDifference, 'days').hours(classTime[1]).minutes(classTime[2]).seconds(0)
 
-    var timeToRing = now.add(dayDifference, 'days').hours(classTime[1]).minutes(classTime[2]).seconds(0)
-
-    if (dayDifference == 0) {
+    if (dayDifference === 0) {
       if (moment().diff(timeToRing) > 0) {
         timeToRing.add(1, 'weeks');
       }
@@ -30,11 +28,11 @@ function createAlarms(entry) {
       timeToRing.subtract(entry.remindTime, 'minutes')
     }
 
-    var startTime = moment.unix(entry.timeBoundaries[0]/1000);
-    var endTime = moment.unix(entry.timeBoundaries[1]/1000);
+    let startTime = moment.unix(entry.timeBoundaries[0]/1000);
+    let endTime = moment.unix(entry.timeBoundaries[1]/1000);
     
     // fudge data to test alarms
-    // var timeObject = new Date();
+    // let timeObject = new Date();
     // target = new Date(timeObject.getTime() + 10000);
 
     console.log([startTime, timeToRing, endTime].join(" | "));
@@ -61,7 +59,7 @@ function editAlarms(zoomerID) {
 
 function deleteAlarm(zoomerID) {
   chrome.alarms.getAll(function (alarms) {
-    for (var i = 0; i < alarms.length; i++) {
+    for (let i = 0; i < alarms.length; i++) {
       if (alarms[i].name.includes(zoomerID)) {
         chrome.alarms.clear(alarms[i].name, function (wasCleared) {
           if (wasCleared) {
